a227935aedc2d8fc6a60197ce7e3aaeef1af1fff
Everton Tenorio <114461163+everton-tenorio@users.noreply.github.com>
2024-01-11 20:49:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -45,6 +45,55 @@ app.get('/', async (req, res) => {
   }
 });
 
+app.get('/jogos', async (req, res) => {
+  try {
+    const response = await axios.get(URL_GOAL);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const json_goal = $('script[id="__NEXT_DATA__"]').text();
+    const json_data = JSON.parse(json_goal);
+    const get_slides = json_data.props.pageProps.content.slideList.slides;
+
+    const serializedData = get_slides.map((data, index) => {
+      const diaJogo = data.headline;
+      const tableHtml = data.body.body;
+      const table = cheerio.load(tableHtml)('table');
+
+      // Remover linhas que contêm a frase "TEMPO REAL"
+      table.find('th:contains("TEMPO REAL")').remove();
+      table.find('td:contains("Tempo real")').remove();
+
+      // Extrair dados individuais
+      const rows = table.find('tbody tr');
+      const serializedContent = rows.map((_, row) => {
+        const columns = $(row).find('td');
+        const jogo = $(columns[0]).text();
+        const campeonato = $(columns[1]).text();
+        const horario = $(columns[2]).text();
+        const ondePassa = $(columns[3]).text();
+
+        return {
+          jogo,
+          campeonato,
+          horario,
+          ondePassa,
+        };
+      }).get();
+
+      return {
+        title: `Data ${index + 1}`,
+        diaJogo,
+        content: serializedContent,
+      };
+    });
+
+    res.json(serializedData);
+  } catch (error) {
+    console.error('Erro ao fazer a requisição:', error);
+    res.status(500).json({ error: 'Erro ao buscar os dados serializados.' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
